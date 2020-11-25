b081c3b5d6491c070cb55f9570f285103680a88c
Kevin Eaton <kevinhowardeaton@gmail.com>
2020-11-25 15:25:07 -0500

    initial game complete

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use bracket_lib::prelude::*;
 
+// set up some constants
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
 // handle the game state
 enum GameMode {
     Menu,
@@ -12,6 +17,8 @@ struct State {
     mode: GameMode,
     player: Player,
     frame_time: f32,
+    obstacle: Obstacle,
+    score: i32,
 }
 
 impl State {
@@ -20,13 +27,36 @@ impl State {
             mode: GameMode::Menu,
             player: Player::new(5,25),
             frame_time: 0.0,
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
+            score: 0,
         }
     }
 
-    // stubs for content
-    // TODO: fill out
+    // the actual game loop
     fn play(&mut self, ctx: &mut BTerm) {
-        self.mode = GameMode::End;
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move()
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap");
+        ctx.print(0, 1, &format!("Score: {}", self.score));
+
+        // set up the obstacles
+        self.obstacle.render(ctx, self.player.x);
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
+
+        if self.player.y > SCREEN_HEIGHT || self.obstacle.hit_obstacle(&self.player) {
+            self.mode = GameMode::End;
+        }
     }
 
     fn main_menu(&mut self, ctx: &mut BTerm) {
@@ -53,8 +83,9 @@ impl State {
     fn dead(&mut self, ctx: &mut BTerm) {
         ctx.cls();
         ctx.print_centered(5, "You lost!");
-        ctx.print_centered(8, "(P) Play");
-        ctx.print_centered(9, "(Q) Quit");
+        ctx.print_centered(7, &format!("Score: {}", self.score));
+        ctx.print_centered(9, "(P) Play");
+        ctx.print_centered(11, "(Q) Quit");
 
         if let Some(key) = ctx.key {
             match key {
@@ -107,7 +138,7 @@ impl Player {
         if self.velocity < 2.0 {
             self.velocity += 0.2;
         }
-        self.y = self.velocity as i32;
+        self.y += self.velocity as i32;
         self.x += 1;
         if self.y < 0 {
             self.y = 0;
@@ -116,6 +147,58 @@ impl Player {
 
     fn flap(&mut self) {
         self.velocity = -2.0;
+    }
+}
+
+// obstacles have gaps and a size that will vary
+struct Obstacle {
+    x: i32,
+    gap_y: i32,
+    size: i32,
+}
+
+impl Obstacle {
+    fn new(x: i32, score: i32) -> Self {
+        let mut random = RandomNumberGenerator::new();
+        Obstacle {
+            x,
+            gap_y: random.range(10, 40),
+            size: i32::max(2, 20 - score),
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm, player_x: i32) {
+        let screen_x = self.x - player_x;
+        let half_size = self.size / 2;
+
+        // top half
+        for y in 0..self.gap_y - half_size {
+            ctx.set(
+               screen_x,
+               y,
+               RGB::named(RED),
+               RGB::named(BLACK),
+               to_cp437('|'), 
+            );
+        }
+        // bottom half
+        for y in self.gap_y + half_size..SCREEN_HEIGHT {
+            ctx.set(
+                screen_x,
+                y,
+                RGB::named(RED),
+                RGB::named(BLACK),
+                to_cp437('|'),
+            );
+        }
+    }
+
+    fn hit_obstacle(&self, player: &Player) -> bool {
+        let half_size = self.size / 2;
+        let does_x_match = player.x == self.x;
+        let player_above_gap = player.y < self.gap_y - half_size;
+        let player_below_gap = player.y > self.gap_y + half_size;
+        does_x_match && (player_above_gap || player_below_gap)
     }
 }
 
