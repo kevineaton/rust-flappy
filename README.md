faee1c1f0b66541d438c2c10a24a5cd14a853a27
Kevin Eaton <kevinhowardeaton@gmail.com>
2020-11-24 23:20:17 -0500

    initial structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flappy (In Rust!)
+
+This repo is for learning purposes only as I follow along in Hands-on Rust. It's not meant as anything other than a place for me to learn (and store) my follow-along code for the book.
+
+As a Go developer most of the time, Rust seems like an interesting language for lower-level coding. It's something I am interested in, working in IoT.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,128 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::*;
+
+// handle the game state
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
+// store the global state
+struct State {
+    mode: GameMode,
+    player: Player,
+    frame_time: f32,
+}
+
+impl State {
+    fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
+            player: Player::new(5,25),
+            frame_time: 0.0,
+        }
+    }
+
+    // stubs for content
+    // TODO: fill out
+    fn play(&mut self, ctx: &mut BTerm) {
+        self.mode = GameMode::End;
+    }
+
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to the game!");
+        ctx.print_centered(8, "(P) Play");
+        ctx.print_centered(9, "(Q) Quit");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+
+    fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
+        self.mode = GameMode::Playing;
+    }
+
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "You lost!");
+        ctx.print_centered(8, "(P) Play");
+        ctx.print_centered(9, "(Q) Quit");
+
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+}
+
+// use the GameState trait from the library
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
+        }
+    }
+}
+
+// the player
+struct Player {
+    x: i32, // world space position, since player is always on the left
+    y: i32, // screen space vertical
+    velocity: f32,
+}
+
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(
+            0,
+            self.y,
+            RGB::named(YELLOW),
+            RGB::named(BLACK),
+            to_cp437('@'),
+        );
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y = self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+
+fn main() -> BError {
+    // build and run, returning the Result
+    let context = BTermBuilder::simple80x50()
+        .with_title("Flappy Rust")
+        .build()?;
+    main_loop(context, State::new())
 }
